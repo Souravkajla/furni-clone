8e07e019d13fb54920587213c775abadda22defb
Souravkajla <159424805+Souravkajla@users.noreply.github.com>
2024-06-03 12:18:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function lessthan() {
+  document.getElementById("dot1").style.color = "";
+  document.getElementById("dot2").style.color = "";
+  document.getElementById("text").style.display = "flex";
+  document.getElementById("text2").style.display = "none";
+  document.getElementById("image").style.display = "flex";
+  document.getElementById("image1").style.display = "none";
+}
+
+function greaterthan() {
+  document.getElementById("dot1").style.color = "#CBD5E1";
+  document.getElementById("dot2").style.color = "black";
+  document.getElementById("text").style.display = "none";
+  document.getElementById("text2").style.display = "flex";
+  document.getElementById("image").style.display = "none";
+  document.getElementById("image1").style.display = "flex";
+}
